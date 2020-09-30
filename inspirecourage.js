07eece809a0876c25f1f9bc116713fabe4a1fc73
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-09-30 20:26:28 +0200

    changed inspirecourage to use item effect

--- inspirecourage.js
+++ inspirecourage.js
@@ -1,15 +1,13 @@
 (async () => {
-    if ((actor.data.data.customModifiers['attack'] || []).some(modifier => modifier.name === 'Inspire Courage')){
+    if (actor.items.find(token => token.data.name === "Spell Effect: Inspire Courage")){
         if (token.data.effects.includes("systems/pf2e/icons/spells/inspire-courage.jpg")){
             await token.toggleEffect("systems/pf2e/icons/spells/inspire-courage.jpg")
         }
-        await actor.removeCustomModifier('attack', 'Inspire Courage');
-        await actor.removeCustomModifier('damage', 'Inspire Courage');
+        await actor.items.find(token => token.data.name === "Spell Effect: Inspire Courage").delete();
     }else{
         if (!token.data.effects.includes("systems/pf2e/icons/spells/inspire-courage.jpg")){
             await token.toggleEffect("systems/pf2e/icons/spells/inspire-courage.jpg")
         }
-        await actor.addCustomModifier('attack', 'Inspire Courage', 1, 'status');
-        await actor.addCustomModifier('damage', 'Inspire Courage', 1, 'status');
+        await actor.createOwnedItem(game.items.getName("Spell Effect: Inspire Courage"));
     }
 })();
