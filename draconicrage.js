354dcf9b0a390e9d8ec9f85f938d81f5091b7e1e
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-11-11 23:43:34 +0100

    fix syntax
    rename pointblankshot

--- draconicrage.js
+++ draconicrage.js
@@ -17,19 +17,19 @@ const imgPath = "systems/pf2e/icons/features/classes/dragon-instinct.jpg";
     if((actor.data.data.customModifiers[modifiers[0].category] || []).some(customModifier => customModifier.name === modifiers[0].name)){
         if (token.data.effects.includes(imgPath)) {
             await token.toggleEffect(imgPath);
-        };
+        }
         for(let modifier of modifiers){
             await actor.removeCustomModifier(modifier.category, modifier.name);
-        };
+        }
         await actor.update({'data.attributes.hp.temp': 0});
     }else{
         if (!token.data.effects.includes(imgPath)) {
             await token.toggleEffect(imgPath);
-        };
+        }
         game.pf2e.rollItemMacro("hHtq5EnPeeQ6tXzK");
         for(let modifier of modifiers){
             await actor.addCustomModifier(modifier.category, modifier.name, modifier.value, modifier.type);
-        };
+        }
         const tempHP = (actor.data.data.details.level.value + actor.data.data.abilities.con.mod);
         if (actor.data.data.attributes.hp.temp < tempHP) {
             await actor.update({'data.attributes.hp.temp': tempHP});
--- inspirecourage.js
+++ inspirecourage.js
@@ -4,12 +4,12 @@ const imgPath = "systems/pf2e/icons/spells/inspire-courage.jpg";
     if (actor.items.find(item => item.data.name === itemName)){
         if (token.data.effects.includes(imgPath)){
             await token.toggleEffect(imgPath);
-        };
+        }
         await actor.items.find(item => item.data.name === itemName).delete();
     }else{
         if (!token.data.effects.includes(imgPath)){
             await token.toggleEffect(imgPath);
-        };
-        await actor.createOwnedItem(game.items.getName(itemName));
+        }
+        await actor.createOwnedItem(game.items.getName("Spell Effect: Inspire Courage"));
     }
 })();
