cd57fc97b490fa7ce8773579dfb3090790d7f82a
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-11-13 00:06:32 +0100

    add layonhands.js

---
+++ layonhands.js
@@ -0,0 +1,32 @@
+const effect = {
+    name: "Lay on Hands",
+    modifier: {
+        stat: "ac",
+        value: 2,
+        type: "status",
+    },
+    iconPath: "systems/pf2e/icons/spells/lay-on-hands.jpg",
+};
+const { _id: itemID, data: { focus: { points: focusPoints, pool: focusPool } } } = await actor.data.items.find(item => item.name === "Focus Spells");
+(async () => {
+    if(event.shiftKey){
+        if(focusPoints < focusPool){
+            actor.updateEmbeddedEntity("OwnedItem",  { _id: itemID, data: { focus: { points: focusPoints + 1, pool: focusPool } } });
+        }else{
+            ui.notifications.warn("Focus pool already full.");
+        }
+    }else if((actor.data.data.customModifiers[effect.modifier.stat] || []).some(customModifier => customModifier.name === effect.name)){
+        if (token.data.effects.includes(effect.iconPath)) {
+            await token.toggleEffect(effect.iconPath);
+        }
+        await actor.removeCustomModifier(effect.modifier.stat, effect.name);
+    }else if(focusPoints <= 0){
+        ui.notifications.warn("You have no focus points left.");
+    }else{
+        if (!token.data.effects.includes(effect.iconPath)) {
+            await token.toggleEffect(effect.iconPath);
+        }
+        await actor.addCustomModifier(effect.modifier.stat, effect.name, effect.modifier.value, effect.modifier.type);
+        actor.updateEmbeddedEntity("OwnedItem",  { _id: itemID, data: { focus: { points: focusPoints - 1, pool: focusPool } } });
+    }
+})();
