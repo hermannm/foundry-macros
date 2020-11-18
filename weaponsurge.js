21525b7dae605d1968c8902c4a6a374c33be03e3
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-11-18 14:54:54 +0100

    expand weaponsurge to use focus points

--- weaponsurge.js
+++ weaponsurge.js
@@ -11,18 +11,41 @@ const effect = {
     },
     iconPath: "systems/pf2e/icons/spells/weapon-surge.jpg",
 };
+const { _id: focusID, data: { focus: { points: focusPoints, pool: focusPool } } } = await actor.data.items.find(item => item.name === "Focus Spells");
 (async () => {
-    if((actor.data.data.customModifiers[effect.modifier.stat] || []).some(customModifier => customModifier.name === effect.name)){
+    if(event.shiftKey){
+        if(focusPoints < focusPool){
+            await actor.updateEmbeddedEntity("OwnedItem",  { _id: focusID, data: { focus: { points: focusPoints + 1, pool: focusPool } } });
+            await ChatMessage.create({
+                user: game.user._id,
+                speaker: ChatMessage.getSpeaker(),
+                content: actor.name + " refocuses, restoring 1 Focus Point.",
+            });
+        }else{
+            ui.notifications.warn("Focus pool already full.");
+        }
+    }else if((actor.data.data.customModifiers[effect.modifier.stat] || []).some(customModifier => customModifier.name === effect.name)){
         if (token.data.effects.includes(effect.iconPath)) {
             await token.toggleEffect(effect.iconPath);
         }
         await actor.removeCustomModifier(effect.modifier.stat, effect.name);
         await actor.removeDamageDice(damageDice.selector, damageDice.name);
+    }else if(focusPoints <= 0){
+        ui.notifications.warn("You have no focus points left.");
     }else{
+        const itemID =
+            actor.items
+                .filter(item => item.data.type === "action")
+                .find(item => item.data.name === effect.name)?._id ??
+            actor.items.find(item => item.data.name === effect.name)?._id;
+        if(itemID){
+            game.pf2e.rollItemMacro(itemID);
+        }
         if (!token.data.effects.includes(effect.iconPath)) {
             await token.toggleEffect(effect.iconPath);
         }
         await actor.addCustomModifier(effect.modifier.stat, effect.name, effect.modifier.value, effect.modifier.type);
         await actor.addDamageDice({ ...effect.damageDice, name: effect.name });
+        await actor.updateEmbeddedEntity("OwnedItem",  { _id: focusID, data: { focus: { points: focusPoints - 1, pool: focusPool } } });
     }
 })();
