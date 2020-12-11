6e95d46b01d3c6b9a1f6f190bad47d3af033970a
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-12-11 21:15:54 +0100

    add critical specialization damage

--- retributionaxe_damage.js
+++ retributionaxe_damage.js
@@ -1,8 +1,4 @@
-const weapon = {
-    name: "Retribution Axe",
-    criticalSpecialization:
-        "Choose one creature adjacent to the initial target and within reach. If its AC is lower than your attack roll result for the critical hit, you deal damage to that creature equal to the result of the weapon damage die you rolled (including extra dice for its potency rune, if any). This amount isn’t doubled, and no bonuses or other additional dice apply to this damage.",
-};
+const weapon = "Retribution Axe";
 const optionalEffect = {
     name: "Retribution",
     description:
@@ -13,26 +9,54 @@ const optionalEffect = {
         type: "circumstance",
     },
 };
+const axeCrit = async (clickEvent) => {
+    const content = game.messages._source[game.messages.size - 1].content;
+    let diceResults = [];
+    const rollIdentifier = `<li class="roll die`;
+    for (
+        let i = content.indexOf(rollIdentifier);
+        i >= 0;
+        i = content.indexOf(rollIdentifier, i + 1)
+    ) {
+        diceResults.push(
+            parseInt(
+                content.slice(
+                    content.indexOf(">", i) + 1,
+                    content.indexOf("</", i)
+                )
+            )
+        );
+    }
+    await DicePF2e.damageRoll({
+        event: clickEvent,
+        parts: diceResults,
+        actor,
+        data: actor.data.data,
+        title: `
+            <div style="line-height:133%">
+                <strong>Critical Specialization</strong> (Axe)
+                <br>
+                Choose one creature adjacent to the initial target and within reach. If its AC is lower than your attack roll result for the critical hit, you deal damage to that creature equal to the result of the weapon damage die you rolled (including extra dice for its potency rune, if any). This amount isn’t doubled, and no bonuses or other additional dice apply to this damage.
+            </div>
+        `,
+        speaker: ChatMessage.getSpeaker(),
+    });
+};
 (async () => {
     const damage = async (crit) => {
+        const clickEvent = event;
         if (crit) {
             await (actor.data.data.actions ?? [])
                 .filter((action) => action.type === "strike")
-                .find((strike) => strike.name === weapon.name)
-                ?.critical(event);
-            if (weapon.criticalSpecialization) {
-                ChatMessage.create({
-                    user: game.user._id,
-                    speaker: ChatMessage.getSpeaker(),
-                    content: `<hr><h3>Critical Specialization</h3><hr>
-                        ${weapon.criticalSpecialization}`,
-                });
-            }
+                .find((strike) => strike.name === weapon)
+                ?.critical(clickEvent);
+            await new Promise(r => setTimeout(r, 200));
+            await axeCrit(clickEvent);
         } else {
             (actor.data.data.actions ?? [])
                 .filter((action) => action.type === "strike")
-                .find((strike) => strike.name === weapon.name)
-                ?.damage(event);
+                .find((strike) => strike.name === weapon)
+                ?.damage(clickEvent);
         }
     };
     const damageWithEffect = async (crit) => {
@@ -49,7 +73,7 @@ const optionalEffect = {
         );
     };
     const dialog = new Dialog({
-        title: `${weapon.name} Damage`,
+        title: `${weapon} Damage`,
         content: `
             <strong>${optionalEffect.name}:</strong> ${optionalEffect.description}<hr>
             <div class="dialog-buttons">
