2109eb40f8a87e927a66ee71f141b16ccf3e6ada
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-12-12 01:49:55 +0100

    add MAP dialog to trip

--- trip.js
+++ trip.js
@@ -2,100 +2,132 @@ const action = {
     name: "Trip",
     skill: "Athletics",
     targetDC: "Reflex",
+    requirements:
+        "You have at least one hand free. Your target can’t be more than one size larger than you.",
+    description:
+        "You try to knock an opponent to the ground. Attempt an Athletics check against the target’s Reflex DC.",
     degreesOfSuccess: {
         criticalSuccess:
             "The target falls and lands prone and takes 1d6 bludgeoning damage.",
         success: "The target falls and lands prone.",
         criticalFailure: "You lose your balance and fall and land prone.",
     },
-    multipleAttackPenalty: true,
+    multipleAttackPenalty: true,    // absent (false), true, or "agile"
 };
 (async () => {
-    let penalty;
-    if (action.multipleAttackPenalty) {
-        if (event.altKey) {
-            penalty = action.multipleAttackPenalty === "agile" ? -4 : -5;
-        } else if (event.ctrlKey) {
-            penalty = action.multipleAttackPenalty === "agile" ? -8 : -10;
-        }
-        if (penalty) {
-            await actor.addCustomModifier(
-                action.skill.toLowerCase(),
-                "Multiple Attack Penalty",
-                penalty,
-                "untyped"
-            );
-        }
-    }
-    const skillKey = Object.keys(actor.data.data.skills).find(
-        (key) => actor.data.data.skills[key].name === action.skill.toLowerCase()
-    );
-    const options = actor.getRollOptions([
-        "all",
-        "skill-check",
-        action.skill.toLowerCase(),
-    ]);
-    actor.data.data.skills[skillKey].roll(event, options, (roll) => {
-        let resultMessage = `<hr /><h3>${action.name}</h3>`;
-        let validTarget = false;
-        for (const target of game.user.targets) {
-            const dc =
-                target.actor?.data?.data?.saves?.[action.targetDC.toLowerCase()]
-                    ?.value + 10;
-            if (dc) {
-                validTarget = true;
-                let successStep =
-                    roll.total >= dc
-                        ? roll.total >= dc + 10
-                            ? 3
-                            : 2
-                        : roll.total > dc - 10
-                        ? 1
-                        : 0;
-                switch (roll.terms[0].results[0].result) {
-                    case 20:
-                        successStep++;
-                        break;
-                    case 1:
-                        successStep--;
-                        break;
-                }
-                resultMessage += `<hr /><b>${target.name}:</b>`;
-                if (successStep >= 3) {
-                    resultMessage += `<br />💥 <b>Critical Success</b>`;
-                    if (action.degreesOfSuccess?.criticalSuccess) {
-                        resultMessage += `<br />${action.degreesOfSuccess.criticalSuccess}`;
+    const skillRoll = () => {
+        const skillKey = Object.keys(actor.data.data.skills).find(
+            (key) => actor.data.data.skills[key].name === action.skill.toLowerCase()
+        );
+        const options = actor.getRollOptions([
+            "all",
+            "skill-check",
+            action.skill.toLowerCase(),
+        ]);
+        actor.data.data.skills[skillKey].roll(event, options, (roll) => {
+            let resultMessage = `<hr /><h3>${action.name}</h3>`;
+            let validTarget = false;
+            for (const target of game.user.targets) {
+                const dc =
+                    target.actor?.data?.data?.saves?.[action.targetDC.toLowerCase()]
+                        ?.value + 10;
+                if (dc) {
+                    validTarget = true;
+                    let successStep =
+                        roll.total >= dc
+                            ? roll.total >= dc + 10
+                                ? 3
+                                : 2
+                            : roll.total > dc - 10
+                            ? 1
+                            : 0;
+                    switch (roll.terms[0].results[0].result) {
+                        case 20:
+                            successStep++;
+                            break;
+                        case 1:
+                            successStep--;
+                            break;
                     }
-                } else if (successStep === 2) {
-                    resultMessage += `<br />✔️ <b>Success</b>`;
-                    if (action.degreesOfSuccess?.success) {
-                        resultMessage += `<br />${action.degreesOfSuccess.success}`;
-                    }
-                } else if (successStep === 1) {
-                    resultMessage += `<br />❌ <b>Failure</b>`;
-                    if (action.degreesOfSuccess?.failure) {
-                        resultMessage += `<br />${action.degreesOfSuccess.failure}`;
-                    }
-                } else if (successStep <= 0) {
-                    resultMessage += `<br />💔 <b>Critical Failure</b>`;
-                    if (action.degreesOfSuccess?.criticalFailure) {
-                        resultMessage += `<br />${action.degreesOfSuccess.criticalFailure}`;
+                    resultMessage += `<hr /><b>${target.name}:</b>`;
+                    if (successStep >= 3) {
+                        resultMessage += `<br />💥 <b>Critical Success</b>`;
+                        if (action.degreesOfSuccess?.criticalSuccess) {
+                            resultMessage += `<br />${action.degreesOfSuccess.criticalSuccess}`;
+                        }
+                    } else if (successStep === 2) {
+                        resultMessage += `<br />✔️ <b>Success</b>`;
+                        if (action.degreesOfSuccess?.success) {
+                            resultMessage += `<br />${action.degreesOfSuccess.success}`;
+                        }
+                    } else if (successStep === 1) {
+                        resultMessage += `<br />❌ <b>Failure</b>`;
+                        if (action.degreesOfSuccess?.failure) {
+                            resultMessage += `<br />${action.degreesOfSuccess.failure}`;
+                        }
+                    } else if (successStep <= 0) {
+                        resultMessage += `<br />💔 <b>Critical Failure</b>`;
+                        if (action.degreesOfSuccess?.criticalFailure) {
+                            resultMessage += `<br />${action.degreesOfSuccess.criticalFailure}`;
+                        }
                     }
                 }
             }
-        }
-        if (validTarget) {
-            ChatMessage.create({
-                user: game.user._id,
-                speaker: ChatMessage.getSpeaker(),
-                content: resultMessage,
-            });
-        }
-    });
-    if (penalty) {
-        actor.removeCustomModifier(
+            if (validTarget) {
+                ChatMessage.create({
+                    user: game.user._id,
+                    speaker: ChatMessage.getSpeaker(),
+                    content: resultMessage,
+                });
+            }
+        });
+    };
+    const skillRollWithMAP = (penalty) => {
+        await actor.addCustomModifier(
+            action.skill.toLowerCase(),
+            "Multiple Attack Penalty",
+            penalty,
+            "untyped"
+        );
+        skillRoll();
+        await actor.removeCustomModifier(
             action.skill.toLowerCase(),
             "Multiple Attack Penalty"
         );
+    }
+    if (action.multipleAttackPenalty) {
+        new Dialog({
+            title: `${action.name}`,
+            content: `
+                <h3>${action.name}</h3><hr>
+                ${action.requirements &&
+                    `<strong>Requirements</strong> ${action.requirements}<hr>`
+                }
+                ${action.description &&
+                    `${action.description}<hr>`
+                }
+            `,
+            buttons: {
+                first: {
+                    label: "1st attack",
+                    callback: skillRoll,
+                },
+                second: {
+                    label: "2nd attack",
+                    callback: () => {
+                        skillRollWithMAP(action.multipleAttackPenalty === "agile" ? -4 : -5);
+                    }
+                },
+                third: {
+                    label: "3rd attack",
+                    callback: () => {
+                        skillRollWithMAP(action.multipleAttackPenalty === "agile" ? -8 : -10);
+                    }
+                }
+            },
+            default: "first",
+        }).render(true);
+    } else {
+        skillRoll();
     }
 })();
