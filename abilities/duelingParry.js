128823bbe013a627c8b2ff0dcef7e2df8640c4d8
hermannm <hermann.morkrid@gmail.com>
2021-03-11 14:43:51 +0100

    add duelingParry.js

---
+++ abilities/duelingParry.js
@@ -0,0 +1,131 @@
+const action = {
+  name: "Dueling Parry",
+  actions: "OneAction",
+  tags: ["Fighter"],
+  requirements:
+    "You are wielding only a single one-handed melee weapon and have your other hand or hands free.",
+  description:
+    "You can parry attacks against you with your one-handed weapon. You gain a +2 circumstance bonus to AC until the start of your next turn as long as you continue to meet the requirements.",
+  effect: {
+    modifier: {
+      stat: "ac",
+      value: 2,
+      type: "circumstance",
+    },
+    iconPath: "systems/pf2e/icons/features/classes/deific-weapon.jpg",
+  },
+};
+(async () => {
+  if (
+    (actor.data.data.customModifiers[action.effect.modifier.stat] || []).some(
+      (customModifier) => customModifier.name === action.name
+    )
+  ) {
+    if (token.data.effects.includes(action.effect.iconPath)) {
+      await token.toggleEffect(action.effect.iconPath);
+    }
+    await actor.removeCustomModifier(action.effect.modifier.stat, action.name);
+  } else {
+    const actionFormat = ({ actions, name, tags, content }) => {
+      const checkTitle = (paragraph) =>
+        typeof paragraph === "object"
+          ? `<strong>${paragraph.title}</strong> ${paragraph.text}`
+          : paragraph;
+      return `
+        <hr style="margin-top: 0; margin-bottom: 3px;" />
+        <header style="display: flex; font-size: 14px">
+          <img
+            style="flex: 0 0 36px; margin-right: 5px;"
+            src="systems/pf2e/icons/actions/${actions}.png"
+            title="${name}"
+            width="36"
+            height="36"
+          >
+          <h3 style="flex: 1; line-height: 36px; margin: 0;">
+            ${name}
+          </h3>
+        </header>
+        ${
+          tags
+            ? `
+              <hr style="margin-top: 3px; margin-bottom: 1px;" />
+              <div class="tags" style="
+                margin-bottom: 5px;
+              ">
+                ${tags
+                  .map(
+                    (tag) => `
+                      <span class="tag tag_alt"">${tag}</span>`
+                  )
+                  .join(" ")}
+              </div>
+            `
+            : `<hr style="margin-top: 3px;" />`
+        }
+        ${
+          content
+            ? `
+              <div style="font-weight: 500; font-size: 14px;">
+                ${content
+                  .map((paragraph) =>
+                    Array.isArray(paragraph)
+                      ? paragraph
+                          .map((subParagraph) => checkTitle(subParagraph))
+                          .join(`<div style="margin-bottom: 5px;"></div>`)
+                      : checkTitle(paragraph)
+                  )
+                  .join("<hr />")}
+              </div>
+            `
+            : ""
+        } 
+      `;
+    };
+    const contentFormat = (action) => {
+      const content = [];
+      if (action.trigger) {
+        content.push({
+          title: "Trigger",
+          text: action.trigger,
+        });
+      }
+      if (action.requirements) {
+        content.push({
+          title: "Requirements",
+          text: action.requirements,
+        });
+      }
+      if (action.description) {
+        content.push(action.description);
+      }
+      if (action.failure) {
+        content.push({
+          title: "Failure",
+          text: action.failure,
+        });
+      }
+      return {
+        actions: action.actions,
+        name: action.name,
+        tags: action.tags,
+        content: content,
+      };
+    };
+    ChatMessage.create({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker(),
+      content: `
+        ${actionFormat(contentFormat(action))}
+      `,
+    });
+    if (!token.data.effects.includes(action.effect.iconPath)) {
+      await token.toggleEffect(action.effect.iconPath);
+    }
+    await actor.addCustomModifier(
+      action.effect.modifier.stat,
+      action.name,
+      action.effect.modifier.value,
+      action.effect.modifier.type
+    );
+  }
+})();
