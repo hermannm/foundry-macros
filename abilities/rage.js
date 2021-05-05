1815d8a440cd29d276f8170f86078cbf52927d65
hermannm <hermann.morkrid@gmail.com>
2021-05-05 21:11:43 +0200

    update rage macro

--- abilities/rage.js
+++ abilities/rage.js
@@ -1,20 +1,16 @@
-const ITEM_UUID = 'Compendium.pf2e.spell-effects.z3uyCMBddrPK5umr'; // Effect: Rage
-
+const ITEM_UUID = "Compendium.pf2e.feature-effects.z3uyCMBddrPK5umr"; // Effect: Rage
 (async () => {
-  const item = await fromUuid(ITEM_UUID);
-  for (const token of canvas.tokens.controlled) {
-    let existing = token.actor.items.filter(i => i.type === item.type).find(e => e.name === item.name);
+  const effect = duplicate(await fromUuid(ITEM_UUID));
+  effect.flags.core = effect.flags.core ?? {};
+  effect.flags.core.sourceId = effect.flags.core.sourceId ?? ITEM_UUID;
+  for await (const token of canvas.tokens.controlled) {
+    let existing = token.actor.items.find(
+      (i) => i.type === "effect" && i.data.flags.core?.sourceId === ITEM_UUID
+    );
     if (existing) {
-      await token.actor.deleteOwnedItem(existing._id);
+      token.actor.deleteOwnedItem(existing._id);
     } else {
-      let owneditemdata = await token.actor.createOwnedItem(item);
-      if (item.type === "effect") {
-        owneditemdata.data.start.value = game.time.worldTime;
-        if (game.combat?.data?.active && game.combat?.turns?.length > game.combat?.turn) {
-            let initiative = game.combat.turns[game.combat.turn].initiative;
-            item.data.start.initiative = initiative;
-        }
-      }
+      token.actor.createOwnedItem(effect);
     }
   }
 })();
